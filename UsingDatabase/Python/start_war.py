5ed02a71ef94a2ab8f9b4ac020d67e1a990d94b8
saharsamr <sahar.rajabi76@gmail.com>
2018-12-22 14:31:40 +0330

    start war

---
+++ UsingDatabase/Python/start_war.py
@@ -0,0 +1,15 @@
+import connect_to_db
+import add_clan
+
+
+def start_war(cnxn):
+    starter = raw_input('Enter starter:') #TODO: ye shakhse khas manteqan bayad betune war ro shoru kone!
+    threatened = raw_input('Enter treathended:')
+
+    cursor = cnxn.cursor()
+
+    command = 'EXEC dbo.StartWar ?, ? '
+
+    cursor.execute(command, (starter, threatened))
+    cnxn.commit()
+
