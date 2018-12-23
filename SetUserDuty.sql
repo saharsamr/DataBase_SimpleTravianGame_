9840a26cd3f8e38090759908ea9adab063973457
saharsamr <sahar.rajabi76@gmail.com>
2018-12-23 12:35:14 +0330

    change user duty bug fixed.

--- SetUserDuty.sql
+++ SetUserDuty.sql
@@ -1,28 +1,37 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE PROCEDURE dbo.SetUserDuty
 		@username VARCHAR(255),
     @duty_name VARCHAR(65)
 AS
 BEGIN
-    DECLARE @duty_id INT;
+    DECLARE @duty_id INT= 0;
+		DECLARE @countUserDuty INT = 0,
+						@exist_temp INT,
+						@exist INT = 1;
 
+	SELECT @exist_temp = COUNT(*) FROM UserData
+		WHERE username = @username;
+	SET @exist = @exist*@exist_temp;
+
+	SELECT @exist_temp = COUNT(*) FROM Duty
+		WHERE duty_name = @duty_name;
+	SET @exist = @exist*@exist_temp;
+
+	IF @exist != 0
+	BEGIN
     SELECT @duty_id = duty_id FROM Duty
         WHERE Duty.duty_name = @duty_name;
 
-		IF NOT EXISTS (SELECT * FROM UserDutyHistory
-										WHERE UserDutyHistory.duty_id = @duty_id
-		  							AND UserDutyHistory.username = @username)
-		BEGIN
+		SELECT @countUserDuty = COUNT(*) FROM UserDutyHistory
+										WHERE duty_id = @duty_id
+		  							AND username = @username
+		IF @countUserDuty = 0
 			INSERT INTO UserDutyHistory (username, duty_id)
 				VALUES (@username, @duty_id)
-		END
 
-    UPDATE UserData
+		UPDATE UserData
 			SET user_current_duty = @duty_id
 				WHERE UserData.username = @username;
+	END
 END
-GO
+go
+
--- UsingDatabase/Python/change_user_duty.py
+++ UsingDatabase/Python/change_user_duty.py
@@ -1,5 +1,5 @@
 def change_user_duty(cnxn):
-    username = raw_input('Enter username:') #TODO: ye shakhse khas manteqan bayad betune war ro shoru kone!
+    username = raw_input('Enter username:')
     duty_name = raw_input('Enter duty name:')
 
     cursor = cnxn.cursor()
@@ -8,4 +8,18 @@ def change_user_duty(cnxn):
 
     cursor.execute(command, (username, duty_name))
     cnxn.commit()
+
+    command = 'SELECT * FROM UserDutyHistory AS UDH ' \
+              'INNER JOIN ' \
+              'Duty AS D ' \
+              'ON UDH.duty_id = D.duty_id ' \
+              'WHERE UDH.username = ? AND ' \
+              'D.duty_name = ?'
+    cursor.execute(command, (username, duty_name))
+
+    if len(cursor.fetchall()) == 0:
+        print 'Not a valid statement'
+    else:
+        print 'User duty updated successfully'
+
 
