4fc90914c07e070dd196470321b31291a5a9bb79
saharsamr <sahar.rajabi76@gmail.com>
2018-12-23 12:17:42 +0330

    set slogan for a clan bug fixed.

--- SetSloganOfClan.sql
+++ SetSloganOfClan.sql
@@ -9,11 +9,25 @@ CREATE PROCEDURE dbo.SetSloganOfClan
 		@doer_username VARCHAR(255)
 AS
 BEGIN
-	IF dbo.HasPermission(@clan_name, @doer_username, 'management_permission') = 1
-    UPDATE Clan
-		    SET slogan = @slogan
-		    WHERE Clan.clan_name = @clan_name;
-	ELSE
-		PRINT 'You do not have permission to set slogan for the clan.'
+	DECLARE @exist_temp INT,
+					@exist INT = 1;
+
+	SELECT @exist_temp = COUNT(*) FROM UserData
+		WHERE username = @doer_username;
+	SET @exist = @exist*@exist_temp;
+
+	SELECT @exist_temp = COUNT(*) FROM Clan
+		WHERE clan_name = @clan_name;
+	SET @exist = @exist*@exist_temp;
+
+	IF @exist != 0
+	BEGIN
+		IF dbo.HasPermission(@clan_name, @doer_username, 'management_permission') = 1
+			UPDATE Clan
+					SET slogan = @slogan
+					WHERE Clan.clan_name = @clan_name;
+		ELSE
+			PRINT 'You do not have permission to set slogan for the clan.'
+	END
 END
 GO
--- UsingDatabase/Python/set_clan_slogan.py
+++ UsingDatabase/Python/set_clan_slogan.py
@@ -10,3 +10,13 @@ def set_clan_slogan(cnxn):
     cursor.execute(command, (clan_name, slogan, doer))
     cnxn.commit()
 
+    command = 'SELECT * FROM Clan ' \
+              'WHERE clan_name = ? AND ' \
+              'slogan = ?'
+    cursor.execute(command, (clan_name, slogan))
+
+    if len(cursor.fetchall()) == 0:
+        print 'Not a valid statement'
+    else:
+        print 'The slogan changed successfully'
+
