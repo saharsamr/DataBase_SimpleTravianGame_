8fd8b05f6abbbf3e9a6e9e3d39a8440f39d5387d
saharsamr <sahar.rajabi76@gmail.com>
2018-12-23 11:05:50 +0330

    remove node js files.

--- UsingDatabase/Backend/routes/create_clan/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const createClan = require('./CreateClan');
-
-router.use('/', createClan);
-
-module.exports = router;
--- UsingDatabase/Backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const createClan = require('./create_clan');
-
-router.use('/createClan', createClan);
-
-module.exports = router;
